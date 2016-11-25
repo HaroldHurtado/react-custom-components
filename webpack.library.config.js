bdec22f193c08f17022a98e417f40bdadd3884b6
Harold Hurtado <movil90hg@gmail.com>
2016-11-25 09:15:23 -0600

    Npm run precompile and modified config in webpack.library.config.js

--- webpack.library.config.js
+++ webpack.library.config.js
@@ -17,6 +17,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
+        include: path.resolve(__dirname, 'src'),
         query: {
           presets: ['react', 'es2015', 'stage-0'],
           plugins: ['react-html-attrs', 'transform-class-properties','transform-decorators-legacy']
